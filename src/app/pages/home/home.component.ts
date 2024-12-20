27516484e71f866051a1f8b253d6c5e45c31cda6
ccaicedo09 <u20231213568@usco.edu.co>
2024-12-19 23:57:40 -0500

    feat: services and others: -import bootstrap icons -navigation through sections -some improvements on redaction

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/shared/navbar/navbar.component';
 import { HeroComponent } from "../../components/home/hero/hero.component";
+import { ServicesComponent } from "../../components/home/services/services.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, ServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
